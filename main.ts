4e9f02dd192d2f20c6de65855333ae0ec9938f50
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2021-03-16 23:32:27 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
-
 input.onButtonPressed(Button.A, function () {
     RainbowSparkleUnicorn.Light.turnOn(lightPins.P8)
 })
 input.onButtonPressed(Button.B, function () {
     RainbowSparkleUnicorn.Light.turnOff(lightPins.P8)
 })
-comment.comment("To use the board you must add this block below as the first block")
+RainbowSparkleUnicorn.comment("To use the board you must add this block below as the first block")
 RainbowSparkleUnicorn.start()
 basic.showNumber(3)
 basic.showNumber(2)
